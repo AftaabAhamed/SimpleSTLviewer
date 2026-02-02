845f36deb7df57298b99ffcb7ecedb6ba37dcdb3
AftaabAhamed <aftaabahamed2003@gmail.com>
2026-02-02 12:18:25 +0530

    Fix: updated to use camera rotation and translation amtrices

--- include/Renderer.hpp
+++ include/Renderer.hpp
@@ -1,6 +1,6 @@
 #pragma once 
 
-
+#include <Eigen/Dense>
 #include <vector>
 #include <array>
 #include <string>
@@ -15,16 +15,16 @@ using points = std::vector<point2d>;
 class Renderer
 {
 private:
-    points project2d(vertices vertices);
+    points project2d(const vertices& vertices);
     std::vector<Mesh> mesh_list;
     Window& window;
-    tf_vec camera_translation;
-    rot_vec camera_rotation;
+    Eigen::Vector3f camera_translation;
+    Eigen::Matrix3f camera_rotation;
 public:
     Renderer(Window &window);
     ~Renderer();
-    vertices translate(vertices ver, const tf_vec &tf_vec);
-    vertices rotate(vertices ver, const rot_vec &rot_vec);
+    // vertices translate(vertices ver, const tf_vec &tf_vec);
+    // vertices rotate(vertices ver, const rot_vec &rot_vec);
     void update_camera_pose(const tf_vec &translation, const rot_vec &rotation);
     void add_mesh(const Mesh &mesh);
     void draw(const Mesh &mesh);
